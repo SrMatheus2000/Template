9d62b2fe2ee7ca8e15fbfa83eafcc1e129b99f0e
Matheus Wandscheer <38194901+SrMatheus2000@users.noreply.github.com>
2019-11-16 15:36:24 -0300

    Includee com switch

--- include/libsql.php
+++ include/libsql.php
@@ -27,25 +27,61 @@
 		return $retorno->fetch();
 	}
 
+	// function includee($a){
+	// 	if ($a == "menu") {
+	// 		include "include/menu.php";
+	// 	} elseif ($a == "cabecalho") {
+	// 		include "include/cabecalho.php";
+	// 	} elseif ($a == "corpo") {
+	// 		include "include/corpo.php";
+	// 	} elseif ($a == "rodape") {
+	// 		include "include/rodape.php";
+	// 	} elseif ($a == "inicio") {
+	// 		include "include/inicio.php";
+	// 	} elseif ($a == "pg1") {
+	// 		include "include/pg1.php";
+	// 	} elseif ($a == "pg2") {
+	// 		include "include/pg2.php";
+	// 	} elseif ($a == "pg3") {
+	// 		include "include/pg3.php";
+	// 	} elseif ($a == "pg4") {
+	// 		include "include/pg4.php";
+	// 	}
+	// }
+
 	function includee($a){
-		if ($a == "menu") {
-			include "include/menu.php";
-		} elseif ($a == "cabecalho") {
-			include "include/cabecalho.php";
-		} elseif ($a == "corpo") {
-			include "include/corpo.php";
-		} elseif ($a == "rodape") {
-			include "include/rodape.php";
-		} elseif ($a == "inicio") {
-			include "include/inicio.php";
-		} elseif ($a == "pg1") {
-			include "include/pg1.php";
-		} elseif ($a == "pg2") {
-			include "include/pg2.php";
-		} elseif ($a == "pg3") {
-			include "include/pg3.php";
-		} elseif ($a == "pg4") {
-			include "include/pg4.php";
+		switch ($a) {
+			case 'menu':
+				include "include/menu.php";
+				break;
+			case 'cabecalho':
+				include "include/cabecalho.php";
+				break;
+			case 'corpo':
+				include "include/corpo.php";
+				break;
+			case 'rodape':
+				include "include/rodape.php";
+				break;
+			case 'inicio':
+				include "include/inicio.php";
+				break;
+			case 'pg1':
+				include "include/pg1.php";
+				break;
+			case 'pg2':
+				include "include/pg2.php";
+				break;
+			case 'pg3':
+				include "include/pg3.php";
+				break;
+			case 'pg4':
+				include "include/pg4.php";
+				break;
+
+			// default:
+			// 	include "include/inicio.php";
+			// 	break;
 		}
 	}
 ?>
